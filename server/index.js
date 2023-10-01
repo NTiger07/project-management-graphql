e9fbedea9dacfb5c5bc71ba45a94e15f90a173b2
NTiger07 <falaleru@gmai.com>
2023-10-01 23:12:37 +0100

    Added Node Scripts

--- server/index.js
+++ server/index.js
@@ -3,4 +3,4 @@ const app = express()
 require("dotenv").config()
 const port = process.env.PORT || 5000
 
-app.listen(port, console.log(""))
+app.listen(port, console.log("Server running on port " + port))
